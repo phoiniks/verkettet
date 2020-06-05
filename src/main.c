32da5ea140e5c47da198884e12ae425c07351979
phoiniks <phoiniks@grellopolis.de>
2020-06-05 21:44:36 +0200

    Dummen Fehler korrigiert

--- src/main.c
+++ src/main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <locale.h>
-
 
 typedef struct person
 {
   char* vorname;
   char* nachname;
-  char* lokalzeit;
   struct person* naechste;
 } person;
 
@@ -19,17 +15,15 @@ typedef struct name
   char* nachname;
 } name;
 
-char* zeit(void);
 void initialisieren(char* vorname, char* nachname, person* p);
 void hinzufuegen(char* vorname, char* nachname, person* p);
 
 int main(int argc, char** argv)
 {
-  setlocale(LC_ALL, "");
-
   person *p = NULL, *kopf = NULL;
 
-  char* personen[][2] = {{"Nuria", "Acacio López"}, {"Laura", "Grell Acacio"}};
+  char* personen[][5] = {{"Nuria", "Acacio López"}, {"Laura", "Grell Acacio"},
+			 {"Frans", "Hals"}, {"Carmen", "Acacio López"}, {"Lucy Maud", "Montgomery"}};
 
   p = malloc(sizeof(person));
   
@@ -40,7 +34,7 @@ int main(int argc, char** argv)
 
   kopf = p;
 
-  for(int i = 0; i < 2; i++)
+  for(int i = 0; i < 5; i++)
     {
       hinzufuegen(personen[i][0], personen[i][1], p);
       p = p->naechste;
@@ -48,47 +42,39 @@ int main(int argc, char** argv)
 
   hinzufuegen("Honoré", "de Balzac", p);
 
-  p = p->naechste;
-
+   p = p->naechste;
+  
   hinzufuegen("Jane", "Austen", p);
+
+   p = p->naechste;
+  
+  hinzufuegen("Carmen", "Acacio López", p);
+
+  /* p->naechste = NULL; */
     
   p = kopf;
 
-  for(int i = 0; i < 5; i++)
+  while(p)
     {
-      printf("%s %s, %s\n", p->vorname, p->nachname, p->lokalzeit);
+      kopf = p;
+      printf("%s %s\n", p->vorname, p->nachname);
       p = p->naechste;
+      free(kopf);
     }  
 }
 
-
-char* zeit(void)
-{
-  time_t jetzt;
-  struct tm* lokaljetzt;
-  char* lokalzeit;
-  
-  time(&jetzt);
-  lokaljetzt = localtime(&jetzt);
-  
-  lokalzeit = malloc(sizeof(char) * 64);
-  strftime(lokalzeit, 64, "%A, %d. %B %Y, %H:%M Uhr und %S Sekunden", lokaljetzt);
-  
-  return lokalzeit;
-}
 
 void initialisieren(char* vorname, char* nachname, person* p)
 {
   p->vorname	= vorname;
   p->nachname	= nachname;
-  p->lokalzeit	= zeit();
 }
 
 void hinzufuegen(char* vorname, char* nachname, person* p)
 {
-  p->naechste			= malloc(sizeof(p));
+  p->naechste   = malloc(sizeof(person));
+  p             = p->naechste;
   
-  p->naechste->vorname  	= vorname;
-  p->naechste->nachname		= nachname;
-  p->naechste->lokalzeit        = zeit();
+  p->vorname  	= vorname;
+  p->nachname	= nachname;
 }
